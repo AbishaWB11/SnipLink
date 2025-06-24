81157590ff77ee7f2fb2a4e629b39cd4ab02f5e3
Abisha Winslet <abishawinslet1101@gmail.com>
2025-06-24 23:17:49 +0530

    Initial commit

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="bg-amber-950"><Bruh></Bruh></div>
+ 
+    </>
+  )
+}
+
+export default App
